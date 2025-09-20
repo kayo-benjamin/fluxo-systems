253f23b70212d5366a783ce3bc9d6e8a8bab956a
Kayo Vinícius <kayo.vinicius034@gmail.com>
2025-09-20 13:22:52 -0300

    Classe de Contrato para o ProdutoDao
    Qualquer classe que queira ser um DAO de Produto precisa ter, no mínimo, os métodos InserirProduto

---
+++ Fluxo/Model/IProdutoDao.cs
@@ -0,0 +1,17 @@
+﻿using Fluxo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluxo.Interface
+{
+    public interface IProdutoDao
+    {
+        void InserirProduto(Produto produto);
+        void AtualizarProduto(Produto produto);
+        void DeletarProduto(Produto produto);
+        List<Produto> ListarProdutos();
+    }
+}
