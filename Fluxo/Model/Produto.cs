080b6b0f252efdd3dccec8a415fbdbc1aba178a5
Kayo Vinícius <kayo.vinicius034@gmail.com>
2025-09-20 13:15:35 -0300

    Classe modelo do Sistema
     Ela tem um ID, um Nome, um Preço e uma Quantidade. É uma classe "burra", serve apenas para carregar dados.

---
+++ Fluxo/Model/Produto.cs
@@ -0,0 +1,22 @@
+﻿using Fluxo.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluxo.Model
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {IdProduto}, Nome: {Nome}, Preço: {Preco}, Quantidade: {Quantidade}";
+        }
+    }
+}
