5962e0f0bf9f23b9643095aeb14d12e8fb73dc34
Kayo Vinícius <kayo.vinicius034@gmail.com>
2025-09-20 13:17:28 -0300

    Auxiliar de Configuração
    Funciona como um "chaveiro" que guarda e nos entrega de forma segura a chave de acesso ao banco de dados (a Connection String) que está no arquivo appsettings.json.

---
+++ Fluxo/Model/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Fluxo.Model
+{
+    public static class AppSettings
+    {
+        private static IConfigurationRoot configuration;
+        static AppSettings()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            configuration = builder.Build();
+        }
+        public static string ConnectionString => configuration.GetConnectionString("DefaultConnection");
+    }
+}
