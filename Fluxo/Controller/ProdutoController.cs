44dd43e9a29cd8b55c68097f3d0911207e24c36e
Kayo Vinícius <kayo.vinicius034@gmail.com>
2025-09-20 13:11:09 -0300

    Fluxo Systems

---
+++ Fluxo/Controller/ProdutoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fluxo.Interface;
+using Fluxo.Model;
+using Npgsql;
+
+namespace Fluxo.Controller
+{
+    public class ProdutoController
+    {
+        private readonly IProdutoDao _produtoDao;
+
+        public ProdutoController(IProdutoDao produtoDao)
+        {
+            _produtoDao = produtoDao;
+        }
+
+        public void InserirProduto(Produto produto)
+        {
+            _produtoDao.InserirProduto(produto);
+        }
+
+        public void AtualizarProduto(Produto produto)
+        {
+            _produtoDao.AtualizarProduto(produto);
+        }
+        public void DeletarProduto(Produto produto)
+        {
+            _produtoDao.DeletarProduto(produto);
+        }
+
+        public List<Produto> ListarProdutos()
+        {
+            return _produtoDao.ListarProdutos();
+        }
+    }
+}
